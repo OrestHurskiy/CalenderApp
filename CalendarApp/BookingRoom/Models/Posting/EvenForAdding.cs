319aa740683fa0c798712c1b777e9784b1129a8d
KarpaDmytro <dkarpa@malkosua.com>
2015-09-24 17:15:52 +0300

    Creating SimpleEvent posting

---
+++ CalendarApp/BookingRoom/Models/Posting/EvenForAdding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingRoom.Models.Posting
+{
+    using Google.Apis.Calendar.v3.Data;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    namespace BookingRoom.Models.Posting
+    {
+        /// <summary>
+        /// Class for Event Adding
+        /// </summary>
+        public class EventForAdding
+        {
+            /// <summary>
+            /// Start Time of the Event
+            /// </summary>
+            public EventTime Start { get; set; }
+            /// <summary>
+            /// End Time of the event
+            /// </summary>
+            public EventTime End { get; set; }
+            /// <summary>
+            /// Title of the event
+            /// </summary>
+            public string Summary { get; set; }
+            /// <summary>
+            /// Description of the event
+            /// </summary>
+            public string Description { get; set; }
+            /// <summary>
+            /// ID of the calendar that will be added
+            /// </summary>
+            public string CalendarID { get; set; }
+        }
+    }
+}
---
+++ CalendarApp/BookingRoom/Models/Posting/EventTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingRoom.Models.Posting
+{
+    public class EventTime
+    {
+        public int Year { get; set; }
+        public int Mounth { get; set; }
+        public int Day { get; set; }
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+}
