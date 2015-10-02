de360ea29776d2d0aac9da3de4eb839635e8b2f0
KarpaDmytro <dkarpa@malkosua.com>
2015-10-02 15:40:18 +0300

    Creating NUnit Test Project

---
+++ CalendarApp/NUnitTestProject/BaseIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using NUnitTestProject.Common;
+using Microsoft.Practices.Unity;
+
+namespace NUnitTestProject
+{
+    [TestFixture]
+    public class BaseIntegrationTest
+    {
+        protected IServiceLocator _serviceLocator;
+        protected IUnityContainer _unityContainer;
+
+        [TestFixtureSetUp]
+        public void Initialize()
+        {           
+            _unityContainer = Dependency.UnityConfig.GetUnityContainer();
+            _serviceLocator = _unityContainer.Resolve<IServiceLocator>();
+        }
+
+    }
+}
